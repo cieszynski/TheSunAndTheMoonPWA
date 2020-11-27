a84411f1f275074afe513d38775a7a3986c0e499
cieszynski <admin@cieszynski.de>
2020-11-27 17:33:52 +0100

    virtualsky added

--- service-worker.js
+++ service-worker.js
@@ -3,34 +3,42 @@
 
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
+const OFFLINE_VERSION = 3;
+const CACHE_NAME = "v7";
+
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "offline.html";
 
 self.addEventListener('install', (event) => {
+  console.log('install');
   event.waitUntil(async function () {
     const cache = await caches.open(CACHE_NAME);
     await cache.addAll([
+      '/'
     ]);
   }());
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(async function() {
+  // it's a good time to handle schema migrations in
+  // IndexedDB and also delete unused caches.
+  console.log('activate');
+  event.waitUntil(async function () {
     const cacheNames = await caches.keys();
     await Promise.all(
       cacheNames.filter((cacheName) => {
-        console.log(cacheName);
+        console.log('activate '+cacheName);
         // Return true if you want to remove this cache,
         // but remember that caches are shared across
         // the whole origin
+        return true;//cacheName !== CACHE_NAME;
       }).map(cacheName => caches.delete(cacheName))
     );
   }());
 });
 
 self.addEventListener('fetch', (event) => {
+  console.log('fetch ' + event.request);
   // A sensible default pattern
   event.respondWith(async function () {
     const cachedResponse = await caches.match(event.request);
